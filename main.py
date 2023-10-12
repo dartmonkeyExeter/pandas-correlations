7160b244d8dff7c4aaee57f844573737cca43a79
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-10-12 14:38:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,40 @@ def correlation_function():
         except FileNotFoundError:
             print('no file with that name found, try again.')
             csv = ''
+    model_choice = ''
+    while not model_choice:
+        model_choice = input('would you like to use a specific model? Y for yes, N for no.\n').lower().strip()
+        if model_choice == 'y':
+            df['model'] = df['model'].str.strip()
+            models = df['model'].to_list()
+            while True:
+                model_name = ''
+                while not model_name:
+                    model_name = input('what model would you like?\n')
+                    if model_name in models:
+                        print('model found!')
+                    else:
+                        print('model not found!')
+                        model_name = ''
+                year = ''
+                while not year:
+                    year = input('from what year?\n').strip()
+                    try:
+                        year = int(year)
+                    except ValueError:
+                        continue
+                df = df[(df == f'{model_name}').any(axis=1) & (df == year).any(axis=1)]
+                df = df.reset_index(drop=True)
+                break
+        elif model_choice == 'n':
+            pass
+        else:
+            print('invalid input, please try again')
+            model_choice = ''
+    calculation(df, csv)
+
+
+def calculation(df, csv):
     cols_to_read = []
 
     allowed = ['year', 'price', 'mileage', 'tax', 'mpg', 'engineSize']
@@ -26,7 +60,7 @@ def correlation_function():
     for i in range(2):
         choice = ''
         while not choice:
-            choice = input(f'column {i+1}: ').strip()
+            choice = input(f'column {i + 1}: ').strip()
             if choice in allowed:
                 cols_to_read.append(choice)
             else:
